824d1005f2f6cc668f8ac94c2878eeee84650e8b
RimJoudi <joudirim93@gmail.com>
2021-04-26 20:39:26 +0100

    edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[-2:]))
-print("Middle word: {}".format(word[1:8]))
+word_3_first = word[:3]
+word_2_last= word[-2:]
+word_middle = word[1:8]
+print("First 3 letters: {}".format(word_3_first))
+print("Last 2 letters: {}".format(word_2_last))
+print("Middle word: {}".format(word_middle))
