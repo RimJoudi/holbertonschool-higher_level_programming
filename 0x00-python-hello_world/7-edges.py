f00337a4a44aff6e7ad5c211cabd470884c6e371
RimJoudi <joudirim93@gmail.com>
2021-04-26 20:42:19 +0100

    edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_3_first = word[:3]
 word_2_last= word[-2:]
-word_middle = word[1:8]
+word_middle = word[1:-1]
 print("First 3 letters: {}".format(word_3_first))
 print("Last 2 letters: {}".format(word_2_last))
 print("Middle word: {}".format(word_middle))
