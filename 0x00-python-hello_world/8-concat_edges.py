77b772f02f5647f20c85a3b53f397da2d5a81a07
RimJoudi <joudirim93@gmail.com>
2021-04-26 18:56:34 +0100

    concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-print(str[38:66] + " " + str[107:111] + " " + str[0:6])
+print(str[38:67] + str[107:112] + str[0:6])
