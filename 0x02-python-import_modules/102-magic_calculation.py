3c907668816950635d3b0581371a672cca21e0cb
RimJoudi <joudirim93@gmail.com>
2021-04-29 18:28:32 +0100

    fix,

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,6 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-    elif:
         for i in range(4, 6):
             c = add(c, i)
             return c
