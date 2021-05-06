af19c66738a75709af452005aacdc40378d8c085
RimJoudi <joudirim93@gmail.com>
2021-05-06 21:49:13 +0100

    best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-    else:
-        return max(a_dictionary, key=a_dictionary.get)
+    return max(a_dictionary, key=a_dictionary.get)
