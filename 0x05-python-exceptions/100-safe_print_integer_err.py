1c60937b0b35328fbbe18111f647045eb9ed83f8
RimJoudi <joudirim93@gmail.com>
2021-05-17 15:10:11 +0100

    add the condition to print in stderr the error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError):
-        print("Exception: Unknown format code 'd' for object of type 'str'")
+    except (TypeError, ValueError) as exp:
+        print("Exception: {}".format(exp),  file=sys.stderr)
         return False
