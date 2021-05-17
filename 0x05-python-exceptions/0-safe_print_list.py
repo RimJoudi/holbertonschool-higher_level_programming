5e3d911af565ccf9daa10f2e89876737e2681db0
RimJoudi <joudirim93@gmail.com>
2021-05-17 10:00:57 +0100

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for i in range(0, x):
+        try:
+            print(my_list[i], end="")
+            i += 1
+        except:
+            break
+    print()
+    return (i)
