4125189cf675ac406ce73f94684eccafeae48124
RimJoudi <joudirim93@gmail.com>
2021-05-17 14:49:04 +0100

    remove (end=)

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         res = None
     finally:
-        print("Inside result: {}".format(res), end="")
+        print("Inside result: {}".format(res))
         return (res)
