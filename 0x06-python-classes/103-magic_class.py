2991a86218f50a8add21f1f8c1daf9104d2d81cc
RimJoudi <joudirim93@gmail.com>
2021-05-18 22:57:16 +0100

    first commit to adv task

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from math import pi
+
+
+class MagicClass:  
+    """
+    class MagicClass
+    """
+    def __init__(self, radius=0):
+        """
+        init method
+        Args:
+            radius (int or float)
+        """
+        if radius is not int:
+            raise TypeError("radius must be a number")
+            
+        if radius is not float:
+            raise TypeError("radius must be a number")
+        else:
+            self._radius = radius
+
+    def area(self):
+        """
+        Instance method
+        Returns:
+            the area of the circule
+        """
+        return ((self._radius ** 2) * pi)
+
+    def circumference(self):
+        """
+        Instance method
+        Returns:
+            the perimeter of the circule
+        """
+        return (2 * pi * self._radius)
