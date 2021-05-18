9b3e136aa780615f966d93114c6c69311fa76c84
RimJoudi <joudirim93@gmail.com>
2021-05-18 19:21:36 +0100

    add module documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+""" module square """
+
+
 class Square:
+    """
+    class Square that defines a square
+    """
     def __init__(self, size):
+        """
+        Init method
+        Args:
+            size (int): size of the square
+        """
         self.__size = size
