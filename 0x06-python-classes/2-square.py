fa999f7cd7172d54125de291fcf6487f2bee6d52
RimJoudi <joudirim93@gmail.com>
2021-05-18 13:21:32 +0100

    first commit to adv task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
