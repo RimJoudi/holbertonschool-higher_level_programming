cc4ad75958db25d6e95fab01b3d8ae754fcec395
RimJoudi <joudirim93@gmail.com>
2021-05-18 19:44:00 +0100

    add module documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+""" module square """
+
+
 class Square:
+    """
+    class Square that defines a square
+    """
     def __init__(self, size=0):
+        """
+        Init method
+        Args:
+            size (int): size of the square
+        Raises:
+            TypeError: if size not an integer
+            ValueError: if size less than 0
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
