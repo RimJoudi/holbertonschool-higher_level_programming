6a848470b2fc04ed68fd08e76eb40227b788512f
RimJoudi <joudirim93@gmail.com>
2021-05-21 22:08:19 +0100

    commit to task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module to print a text with 2 new lines
+"""
+def text_indentation(text):
+    """
+    a function that prints a text with 2 new lines/
+    after each of these characters: ., ? and :
+    Args:
+        text(str): the given text
+    Raises:
+        TypeError: if the text is not str
+    Returns:
+        the new text
+    
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in ":.,?":
+            print('\n')
+            if i == len(text) - 1:
+                break
+            if text[i + 1] == ' ':
+                i += 1
+            while text[i] == ' ' and text[i + 1] == ' ' and i + 1 < len(text):
+                i += 1
+        i += 1
