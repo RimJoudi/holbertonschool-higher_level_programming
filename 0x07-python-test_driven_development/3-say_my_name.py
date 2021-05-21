3ed1a45190f6aa3c8b6342cc6bbeef503e04ae96
RimJoudi <joudirim93@gmail.com>
2021-05-21 20:54:47 +0100

    add module doc + pep8 fixed

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module to print first_name and last_name
+"""
+
+
 def say_my_name(first_name, last_name=""):
     """
     function that prints My name is <first name> <last name>
