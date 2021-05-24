6d6cb163947e0a5903703d6384ff3d37e926eba5
RimJoudi <joudirim93@gmail.com>
2021-05-24 01:28:00 +0100

    fix pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,6 +2,8 @@
 """
 add 2 integers module
 """
+
+
 def add_integer(a, b=98):
     """
     function that adds 2 integers.
@@ -13,12 +15,12 @@ def add_integer(a, b=98):
     Returns:
         an integer equal to the addition of a and b
     """
-    if type(a) != int and type (a) != float:
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    if type(b) != int and type (b) != float:
+    if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
     if type(a) == float:
-        a = int (a)
+        a = int(a)
     if type(b) == float:
         b = int(b)
     return a + b
