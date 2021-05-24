bd7c84f9951a440b0bf1056ceeec1e67b339fa2c
RimJoudi <joudirim93@gmail.com>
2021-05-24 15:03:30 +0100

    add @staticmethod decorator

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -107,6 +107,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
         returns the biggest rectangle based on the area
