ee95c5afcd053ada3465881cebbca8367d950330
RimJoudi <joudirim93@gmail.com>
2021-06-01 12:08:30 +0100

    commit to task0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def lookup(obj):
+    return dir(obj)
+
+
+
+
+
+
+
+
+'''
+Write a function that returns the list of available attributes and methods of an object:
+
+    Prototype: def lookup(obj):
+    Returns a list object
+    You are not allowed to import any module
+
+'''
