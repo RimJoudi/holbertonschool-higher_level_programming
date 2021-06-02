f4163497580106e3ff494146b49f5e7a3391cede
RimJoudi <joudirim93@gmail.com>
2021-06-02 23:26:31 +0100

    commit to task8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+class_to_json module
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description
+    with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    """
+    return obj.__dict__
