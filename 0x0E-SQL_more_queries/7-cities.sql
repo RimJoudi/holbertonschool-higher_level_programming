a6cf790fa587971a7dfa41fc672a3b677aaee25d
RimJoudi <joudirim93@gmail.com>
2021-07-13 20:29:27 +0100

    commit to task7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- creates database hbtn_0d_usa and cities table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
