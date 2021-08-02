dc84c10e5109af7093f528ca4f0c581f94e3e19e
RimJoudi <joudirim93@gmail.com>
2021-08-02 22:59:52 +0100

    add semicolons at the end of declaration

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for (let i = 0; i < 3; i++) {
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < array.length; i++) {
 	console.log(array[i]);
 }
