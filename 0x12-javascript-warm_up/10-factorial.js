016fdd25e293e02967f409ad77f0934d9c7f835a
RimJoudi <joudirim93@gmail.com>
2021-08-03 11:35:02 +0100

    fix the semistandard styling

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const n = Number(process.argv[2]);
 function factorial (n) {
-    if (n === 1) {
-      return (1);
-    }
-    return (n * factorial(n - 1));
+  if (n === 1) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
 }
 if (isNaN(n)) {
-    console.log(1);
+  console.log(1);
 } else {
-    console.log(factorial(n));
+  console.log(factorial(n));
 }
