8f984d44c61af1910defd449c55a1fe0e4e15296
RimJoudi <joudirim93@gmail.com>
2021-08-08 20:57:40 +0100

    fix pep8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,9 @@ def filter_states():
                          passwd=password, db=database)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states\
+                WHERE name LIKE BINARY 'N%'\
+                ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
