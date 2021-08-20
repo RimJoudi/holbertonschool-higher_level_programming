d563925bd3ba92ea3c727f6582570620e875c3f3
RimJoudi <joudirim93@gmail.com>
2021-08-20 15:08:54 +0100

    remove white space

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,10 @@ https://intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == '__main__':
-
+    
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         print("Body response:")
         html = response.read()
-        print("\t- type: ", type(html))
+        print("\t- type:", type(html))
         print("\t- content:", html)
         print("\t- utf8 content:", html.decode('utf-8'))
