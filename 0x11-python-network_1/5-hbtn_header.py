ebdc836a0ac825e9cca9379192d1f88640919fce
RimJoudi <joudirim93@gmail.com>
2021-08-20 23:05:44 +0100

    commit to task4, 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the variable X-Request-Id
+in the response header
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
